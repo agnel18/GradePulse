c2b0ed320c726787f0b06834a21caef12dc39fbf
agnel18 <agnel.ajrocker@gmail.com>
2025-11-10 16:05:36 +0530

    feat: Spring Boot 3.4.11 + 9 pro dependencies (Zoho-level stack)

---
+++ src/main/java/com/gradepulse/GradePulseApplication.java
@@ -0,0 +1,13 @@
+package com.gradepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradePulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradePulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradepulse/GradePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradePulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
